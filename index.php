a589f114de0553d6ce9b8551e8184d5221952bc3
Dani9807 <107667953+Dani9807@users.noreply.github.com>
2022-06-16 23:33:47 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,143 @@
+<html Lang="es">
+    <head>
+        <title>Reportes de alumnos</title>
+    </head>
+     <body bgcolor="#6ED613">
+     <center>
+         <h1>REPORTES DE ALUMNOS</h1>
+         <h2>¿y como funciona esto? seleccione alguno de estos numeros de control y sera mandado al archivo pdf que desea </h2>
+         <h4>Por manuel daniel olloqui rodriguez-18410762</h4>
+         <a href="#" target="_self"></a>
+         <form>
+             
+             <h4>En caso de que no reconoce ningun numero revise esta lista(los numero de control de algunos no fueron encontrados):</h4>
+             <select>
+                 <optgroup label="Residencia_0001">
+                 <option>Myriam Guadalupe Mariquez Sanchez-0541078</option>
+                 <option>Maria Anabel Vargas Rivera-05410648</option>
+                 </optgroup>
+                 <optgroup label="Residencia_0003">
+                 <option>Leonel Ramirez Saenz-05410079</option>
+                 <option>Daniel Angel Contreras Zaragoza-05410202</option>
+                 <option>Mariel Heredia Lucero-05410054</option>
+                 <option>Rocio Margarita Rivera Botello-05410120</option>
+                 </optgroup>
+                 <optgroup label="Residencia_0005">
+                 <option>Alma Adriana Ontiveros Marquez-04410165</option>
+                 <option>Nubia Lucero Hernandez-04410159</option>
+                 </optgroup>
+                 <optgroup label="Residencia_0007">
+                 <option>Hernandez Cano Elva Luz-02410459</option>
+                 <option>Rodriguez Montoya Jessica Ivette-03410020</option>
+                 </optgroup>
+                 <optgroup label="Residencia_0009">
+                 <option>Meza Urgarte Isabel Cristina-03410017</option>
+                 <option>Palma Duarte Jitzel Denisse-02410916</option>
+                 </optgroup>
+                 <optgroup label="Residencia_0011">
+                 <option>Porras Muñoz Maria Guadalupe-04410208</option>
+                 <option>Lerma Luna Pedro Severiano-04410646</option>
+                 <option>Hernandez Peña Jaime Azair-04410856</option>
+                 </optgroup>
+                 <optgroup label="Residencia_0013">
+                 <option>Jose de Jesus Gonzalez Lopez-</option>
+                 <option>Martin Alberto Torres Lujan-</option>
+                 </optgroup>
+                 <optgroup label="Residencia_0015">
+                 <option>Jesus Esteban Lopez Villalobos-</option>
+                 <option>Jose Alfredo Cota Batista-</option>
+                 </optgroup>
+                 <optgroup label="Residencia_0017">
+                 <option>Nancy Jalohany Ojeda Garcia-</option>
+                 <option>Daniel Gonzalo Reyes Torres-</option>
+                 </optgroup>
+                 <optgroup label="Residencia_0019">
+                 <option>Hector Adrian Estrada Cañas-</option> 
+                 <option>Yrelda Angelica Zuñiga Carrillo-</option>
+                 </optgroup>
+                 <optgroup label="Residencia_0021">
+                 <option>Octavio Andres Cardenas Larrea-</option>
+                 </optgroup>
+                 <optgroup label="Residencia_0023">
+                 <option>Gustavo Sandoval Montes-</option>
+                 </optgroup>               
+             </select>             
+         </form>        
+     </center> 
+     <table>
+         <tr>
+        <td> <a class="boton" href="Archivospdf/Residencia_0001.pdf" target="_blank">0541078</a> </td> 
+      
+        <td> <a class="boton" href="Archivospdf/Residencia_0001.pdf" target="_blank">05410648</a> </td> 
+        </tr>
+    </table>
+     <table>
+         <tr>
+             <td><a class="boton" href="Archivospdf/Residencia_0003.pdf" target="_blank">05410079</a> </td>
+             <td><a class="boton" href="Archivospdf/Residencia_0003.pdf" target="_blank">05410202</a></td>
+             <td><a class="boton" href="Archivospdf/Residencia_0003.pdf" target="_blank">05410054</a></td>
+             <td><a class="boton" href="Archivospdf/Residencia_0003.pdf" target="_blank">05410120</a></td>
+        </tr>
+    </table>
+     <table>
+         <tr>
+          <td><a class="boton" href="Archivospdf/Residencia_0005.pdf" target="_blank">04410165</a></td>
+          <td><a class="boton" href="Archivospdf/Residencia_0005.pdf" target="_blank">04410159</a></td>
+         </tr>
+     </table>
+     <table>
+         <tr>
+             <td><a class="boton" href="Archivospdf/Residencia_0007.pdf" target="_blank">02410459</a></td>
+             <td><a class="boton" href="Archivospdf/Residencia_0007.pdf" target="_blank">03410020</a></td>
+         </tr>
+     </table>
+     <table>
+         <tr>
+         <td> <a class="boton" href="Archivospdf/Residencia_0009.pdf" target="_blank">03410017</a> </td>
+         <td> <a class="boton" href="Archivospdf/Residencia_0009.pdf" target="_blank">02410916</a> </td> 
+         </tr>
+     </table>
+     <table>
+         <tr>
+         <td> <a class="boton" href="Archivospdf/Residencia_0011.pdf" target="_blank">04410208</a> </td>
+         <td> <a class="boton" href="Archivospdf/Residencia_0011.pdf" target="_blank">04410646</a> </td>
+         <td> <a class="boton" href="Archivospdf/Residencia_0011.pdf" target="_blank">04410856</a> </td>
+         </tr>
+     </table>
+     <table>
+         <tr>
+             
+         </tr>
+     </table>
+     <table>
+         <tr>
+             <td> <a class="boton" href="Archivospdf/Residencia_0013.pdf" target="_blank">Jose de Jesus Gonzalez Lopez</a> </td>
+             <td> <a class="boton" href="Archivospdf/Residencia_0013.pdf" target="_blank">Martin Alberto Torres Lujan</a> </td>
+         </tr>
+     </table>
+     <table>
+         <tr>
+             <td> <a class="boton" href="Archivospdf/Residencia_0015.pdf" target="_blank">Jesus Esteban Lopez Villalobos</a> </td>
+             <td> <a class="boton" href="Archivospdf/Residencia_0015.pdf" target="_blank">Jose Alfredo Cota Batista</a> </td>
+         </tr>
+     </table>
+     <table>
+         <tr>
+             <td> <a class="boton" href="Archivospdf/Residencia_0019.pdf" target="_blank">Hector Adrian Estrada Cañas</a> </td>
+             <td> <a class="boton" href="Archivospdf/Residencia_0019.pdf" target="_blank">Yrelda Angelica Zuñiga Carrillo</a> </td>
+         </tr>
+     </table>
+     <table>
+         <tr>
+             <td> <a class="boton" href="Archivospdf/Residencia_0021.pdf" target="_blank">Octavio Andres Cardenas Larrea</a> </td>
+         </tr>
+     </table>
+     <table>
+         <tr>
+             <td> <a class="boton" href="Archivospdf/Residencia_0023.pdf" target="_blank">Gustavo Sandoval Montes</a> </td>
+         </tr>
+     </table>
+     
+     
+</body>
+</html>
